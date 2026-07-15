3a9b74a46128a35e9ca62526813e188e90b623fb
AleksandrTeslenko <at@innotech-ua.com>
2026-07-15 16:13:55 +0300

    change data

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
     // useBackend: true,
     useBackend: false,
-    client: 'Tocan',
-    clientLogo: 'logo_tocan_orange.svg'
+    client: '',
+    clientLogo: 'apple-touch-icon.png'
 };
